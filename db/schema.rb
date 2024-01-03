6ae1b925dd7ce3f753f8b368ce72c5d03200d0f7
Patryk Wawrzacz <tatrenings@gmail.com>
2024-01-03 14:06:24 +0100

    icons and banner changes

--- db/schema.rb
+++ db/schema.rb
@@ -73,12 +73,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_28_151242) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
